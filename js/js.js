47ad4289919b605e7deaa99aff207a1be6b7e301
vaisy <40660896+vaisy@users.noreply.github.com>
2019-06-08 23:39:22 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $("#menu-icon").attr('src', 'img/menu-open.png');
+    $(".nav-mobile-menu").hide();
+
+    $('#menu-icon').click(function () {
+        if ($("#menu-icon").attr('src') === 'img/menu-open.png') {
+            $("#menu-icon").attr('src', 'img/menu-close.png');
+            $(".nav-mobile-menu").slideDown();
+        }
+        else {
+
+            $("#menu-icon").attr('src', 'img/menu-open.png');
+            $(".nav-mobile-menu").slideUp();
+        }
+    });
+});
+function ShowMoreText(button, dots, moretext) {
+    var dots_new = document.getElementById(dots);
+    var moretext_new = document.getElementById(moretext);
+    var showbtn = document.getElementById(button);
+
+    if (moretext_new.style.display === "none" || moretext_new.style.display === "") {
+        moretext_new.style.display = "inline";
+        dots_new.style.display = "none";
+        showbtn.innerText = "Read less";
+    }
+    else {
+        moretext_new.style.display = "none";
+        dots_new.style.display = "inline";
+        showbtn.innerText = "Read more";
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,19 @@
+# Gul Awaiz - Web Developer
+
+## Introduction
+
+This website project is to showcase the ability to create a website using only HTML, Css and Javascript/ jQuery. This website explain who I am and what I can do. 
+
+It is responsive website and uses bootstrap styles and script to make it responsive. The menu of the website is also responsive you can resize the screen to check the menu behaviour changing when the screen size is changed. 
+
+It contains four html pages named, index.html, resume.html, portfolio.html and contact.html. The stylesheet and script are in separate files named as style.css and js.js.
+
+
+
+## Code Samples
+
+//.................................................................//
+
+## Installation
+
+//.................................................................//
